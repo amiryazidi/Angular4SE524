a869b10d863e62f295b7e6fbd11efd48c29ba786
Amir YAZIDI <amir.yazidi1@esprit.tn>
2024-02-15 15:27:00 +0100

    formulaire

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ResidenceComponent } from './residence/residence.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'home', component:HomeComponent},
+//route par defaut
+{path:'', redirectTo:'/home', pathMatch:'full'},
+{path:'product', component:ProductComponent},
+{path:'residence', component:ResidenceComponent},
+{path:'form', component:FormComponent},
+{path:'product/:id',component:DetailProductComponent},
+//route NotFound
+{path:'**',component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
